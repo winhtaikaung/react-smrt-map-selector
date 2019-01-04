90bb5a46cbbe60d7b01a604b2cc5bf354fe7c7f3
winhtaikaung <winhtaikaung28@hotmail.com>
2019-01-04 16:47:42 +0800

    docs mrt fixed

---
+++ docs/build/precache-manifest.ab1f865cd2e2908038dd0dc85023283c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "087378f7ce22a07b06e6",
+    "url": "/static/js/main.087378f7.chunk.js"
+  },
+  {
+    "revision": "118a83df79f422e4cbed",
+    "url": "/static/js/1.118a83df.chunk.js"
+  },
+  {
+    "revision": "087378f7ce22a07b06e6",
+    "url": "/static/css/main.e8c71a85.chunk.css"
+  },
+  {
+    "revision": "cf342ce47787d65a1b4b17a6b084c2a7",
+    "url": "/index.html"
+  }
+];
