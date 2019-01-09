f4f091637578c66e58a7ea3147021414a0d81471
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2019-01-09 08:23:36 +0800

    docs deployment updated

---
+++ docs/precache-manifest.d56ac8c2abe75ecd4bf320794d82748a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8742d0ce29976c1b7034",
+    "url": "/static/js/main.8742d0ce.chunk.js"
+  },
+  {
+    "revision": "137ecff73920e8fe614d",
+    "url": "/static/js/1.137ecff7.chunk.js"
+  },
+  {
+    "revision": "8742d0ce29976c1b7034",
+    "url": "/static/css/main.efc248b4.chunk.css"
+  },
+  {
+    "revision": "ca07548f6f6825aef2d6be5e8c5224df",
+    "url": "/index.html"
+  }
+];
