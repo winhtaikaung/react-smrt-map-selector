6b34d9b8044702e34cef39e9a700db0510e12db2
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2019-01-15 00:13:26 +0800

    removed unused css and html removed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import React, { Component } from 'react';
 
-import './App.css';
-import SGMrtSelector from './SgMrtSelector';
+import SmrtSelector from './SmrtSelector';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <SGMrtSelector 
+      <SmrtSelector 
       selectedStations={[]} 
       onStationsCheckChange={(item,selectedStn)=>{
         console.log(item,selectedStn)
       }}
-      displayStations={["EW_LINE_STN","NS_LINE_STN","NE_LINE_STN","CC_LINE_STN","DTL_LINE_STN","BP_LRT_LINE_STN","NS_SK_LRT_LINE_STN","NS_PG_LRT_LINE_STN"]}
-      isDisplayFutureStation={false}
+      config={{backgroundColor:'#FFF',textColor:'#000000',title:{textColor:"#000000"}}}
+      // displayStations={["EW_LINE_STN","NS_LINE_STN","NE_LINE_STN","CC_LINE_STN","DTL_LINE_STN","BP_LRT_LINE_STN","NS_SK_LRT_LINE_STN","NS_PG_LRT_LINE_STN"]}
+      isDisplayFutureStation={true}
+      displayTagSelector={true}
       
       />
-        <header className="App-header">
-          
-          
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Singapore SMRT Map Selector
-          </a>
-        </header>
+        
       </div>
     );
   }
