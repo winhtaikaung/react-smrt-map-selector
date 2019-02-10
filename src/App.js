a61136551094f247b9816dd91afab607efbb6139
win <winhtaikaung28@gmail.com>
2019-02-10 11:18:21 +0800

    moved constants from gen Image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import SmrtSelector from "./SmrtSelector";
+import SmrtSelector from './SmrtSelector';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SmrtSelector
-          onStationsCheckChange={(item, selectedStn) => {
-            
-          }}
-          config={{
-            backgroundColor: "#FFF",
-            textColor: "#000000",
-            title: { textColor: "#000000" }
-          }}
           // displayStations={["EW_LINE_STN","NS_LINE_STN","NE_LINE_STN","CC_LINE_STN","DTL_LINE_STN","BP_LRT_LINE_STN","NS_SK_LRT_LINE_STN","NS_PG_LRT_LINE_STN"]}
           isDisplayFutureStation={true}
-          
         />
       </div>
     );
